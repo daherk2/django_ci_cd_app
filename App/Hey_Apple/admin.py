c7d5390a94a87dadb93716470a84c103ba3b3ff4
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2019-03-06 15:04:36 -0300

    Teste de Deploy - Add ADMINREG

--- App/Hey_Apple/admin.py
+++ App/Hey_Apple/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Documento
+
+@admin.register(Documento)
+class MyClientFeatures(admin.ModelAdmin):
+    search_fields = ("nome", "tipo")
+    list_display = ("nome", "tipo")
